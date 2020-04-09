358b141da8ecb4e3908b95af9726bac9ca974041
Oliver Bajus <bajuso@spse-po.sk>
2020-04-09 19:45:59 +0200

    Profile fragment a Add product layout

--- app/src/main/java/com/spse/decusproject/PopActivity.java
+++ app/src/main/java/com/spse/decusproject/PopActivity.java
@@ -2,57 +2,108 @@ package com.spse.decusproject;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.decus.R;
+import com.google.android.gms.common.data.BitmapTeleporter;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 
-public class PopActivity extends Activity {
-    EditText password;
-    Button changeButon;
+public class PopActivity extends Activity implements DatePickerDialog.OnDateSetListener {
+    EditText productName;
+    Button addProduct;
+    Spinner spinner;
+    private TextView dateText;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pop);
-        changeButon=findViewById(R.id.btnChangePassword);
-        password  = findViewById(R.id.passwordForChange);
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_pop);
 
-        DisplayMetrics dm= new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getRealMetrics(dm);
+            productName=findViewById(R.id.productName);
+            addProduct = findViewById(R.id.addProduct);
+            spinner = findViewById(R.id.spinner);
+            dateText = findViewById(R.id.date_text);
 
-        int width= dm.widthPixels;
-        int height =dm.heightPixels;
+            DisplayMetrics dm= new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getRealMetrics(dm);
 
-        getWindow().setLayout((int)(width*.6),(int)(height*.3));
+            int width= dm.widthPixels;
+            int height =dm.heightPixels;
 
-        WindowManager.LayoutParams params=getWindow().getAttributes();
-        params.gravity= Gravity.CENTER;
-        params.x=0;
-        params.y=-20;
+            getWindow().setLayout((int)(width*.8),(int)(height*.6));
 
-        getWindow().setAttributes(params);
+            WindowManager.LayoutParams params=getWindow().getAttributes();
+            params.gravity= Gravity.CENTER;
+            params.x=0;
+            params.y=-20;
 
-        changeButon.setOnClickListener(new View.OnClickListener() {
+            getWindow().setAttributes(params);
+            getWindow().setAttributes(params);
+            List<String> list= new ArrayList<>();
+            list.add("Acids");
+            list.add("Masks");
+            list.add("Cleaners");
+            list.add("Moisturizes");
+            list.add("Oils");
+
+            ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+            arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(arrayAdapter);
+
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    String itemValue=parent.getItemAtPosition(position).toString();
+
+                    Toast.makeText(PopActivity.this,"Selected"+itemValue, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+        findViewById(R.id.date_text).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                showDatePickerDialog();
             }
         });
     }
+        public void showDatePickerDialog(){
+               DatePickerDialog datePickerDialog = new DatePickerDialog(this,  this,Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH) , Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+               datePickerDialog.show();
+
+         }
 
 
-}
+       public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date = "   Expiration date: " + month + "/" + dayOfMonth + "/" + year;
+        dateText.setText(date);
+    }
+   }
--- app/src/main/java/com/spse/decusproject/ProfileFragment.java
+++ app/src/main/java/com/spse/decusproject/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.spse.decusproject;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,6 +16,7 @@ import com.example.decus.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -24,10 +27,12 @@ import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
-    TextView fullName,email,changePassword;
+    TextView fullName,email,changePassword,changeEmail;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
+    FirebaseUser user;
     String userId;
+    Button logOut,addAllergen,addProduct;
 
 
     @Nullable
@@ -144,6 +149,14 @@ public class ProfileFragment extends Fragment {
 
             }
         });
+
+        addProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity().getApplicationContext(), PopActivity.class));
+
+            }
+        });
     }
 
 
@@ -155,22 +168,16 @@ public class ProfileFragment extends Fragment {
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         userId = fAuth.getCurrentUser().getUid();
+        logOut=view.findViewById(R.id.logout);
+        changePassword=view.findViewById(R.id.changePassword);
+        changeEmail=view.findViewById(R.id.changeEmail);
+        addAllergen = view.findViewById(R.id.addAllergen);
+        addProduct = view.findViewById(R.id.addProduct);
     }
 
-    public void logout(View view) {
-        FirebaseAuth.getInstance().signOut();//logout
-        startActivity(new Intent(getActivity().getApplicationContext(),Login.class));
-        getActivity().finish();
-    }
 
 
-    public void changePassword(View view) {
-        Intent i=new Intent(getActivity().getApplicationContext(),PopActivity.class);
-        startActivity(i);
-    }
 
-    public void changeEmail(View view) {
-    }
 
 
 
