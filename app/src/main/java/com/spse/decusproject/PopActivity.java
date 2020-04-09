55739bdfea3903272b280a08646774385d0a67a9
Oliver Bajus <bajuso@spse-po.sk>
2020-04-09 21:23:21 +0200

    Funkcne pridavanie produktov do databazy

--- app/src/main/java/com/spse/decusproject/PopActivity.java
+++ app/src/main/java/com/spse/decusproject/PopActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
@@ -24,6 +26,8 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -33,20 +37,31 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 public class PopActivity extends Activity implements DatePickerDialog.OnDateSetListener {
-    EditText productName;
+    EditText productName,productBrand;
     Button addProduct;
     Spinner spinner;
     private TextView dateText;
+    String date;
+
+    DatabaseReference databaseProducts;
+
+    FirebaseAuth fAuth;
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_pop);
 
-            productName=findViewById(R.id.productName);
-            addProduct = findViewById(R.id.addProduct);
+            productName = findViewById(R.id.productName);
+            productBrand = findViewById(R.id.productBrand);
+            addProduct = findViewById(R.id.btnAddProduct);
             spinner = findViewById(R.id.spinner);
             dateText = findViewById(R.id.date_text);
+
+            fAuth = FirebaseAuth.getInstance();
+
+
+            databaseProducts = FirebaseDatabase.getInstance().getReference("products");
 
             DisplayMetrics dm= new DisplayMetrics();
             getWindowManager().getDefaultDisplay().getRealMetrics(dm);
@@ -74,28 +89,49 @@ public class PopActivity extends Activity implements DatePickerDialog.OnDateSetL
             arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinner.setAdapter(arrayAdapter);
 
-            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    String itemValue=parent.getItemAtPosition(position).toString();
-
-                    Toast.makeText(PopActivity.this,"Selected"+itemValue, Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onNothingSelected(AdapterView<?> parent) {
-
-                }
-            });
-
         findViewById(R.id.date_text).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePickerDialog();
             }
         });
+
+        addProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addProduct();
+            }
+        });
+
+
     }
-        public void showDatePickerDialog(){
+
+    private void addProduct() {
+        String name=productName.getText().toString().trim();
+        String brand=productBrand.getText().toString().trim();
+        String category = spinner.getSelectedItem().toString();
+        String productDate=dateText.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(name)){
+            if (!TextUtils.isEmpty(brand)){
+                if (!productDate.contains("Choose expiration date.")){
+                        String id=databaseProducts.push().getKey();
+                        Product product = new Product(name,brand,category,date,fAuth.getCurrentUser().getUid());
+
+                        databaseProducts.child(id).setValue(product);
+
+                    Toast.makeText(PopActivity.this,"Product added.",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                else Toast.makeText(PopActivity.this,"Choose product expiration date.",Toast.LENGTH_LONG).show();
+            }
+            else Toast.makeText(PopActivity.this,"Enter product brand.",Toast.LENGTH_LONG).show();
+        }
+        else Toast.makeText(PopActivity.this,"Enter product name.",Toast.LENGTH_LONG).show();
+
+    }
+
+    public void showDatePickerDialog(){
                DatePickerDialog datePickerDialog = new DatePickerDialog(this,  this,Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH) , Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
                datePickerDialog.show();
 
@@ -103,7 +139,7 @@ public class PopActivity extends Activity implements DatePickerDialog.OnDateSetL
 
 
        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        String date = "   Expiration date: " + month + "/" + dayOfMonth + "/" + year;
-        dateText.setText(date);
+        date = month + "/" + dayOfMonth + "/" + year;
+        dateText.setText("   Expiration date: " +date);
     }
    }
---
+++ app/src/main/java/com/spse/decusproject/Product.java
@@ -0,0 +1,38 @@
+package com.spse.decusproject;
+
+import android.content.Intent;
+
+import java.util.Date;
+
+public class Product {
+    String name,brand,category,date,userID;
+
+
+    public Product(String name, String brand, String category, String date, String userID) {
+        this.name = name;
+        this.brand = brand;
+        this.category = category;
+        this.date = date;
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+}
